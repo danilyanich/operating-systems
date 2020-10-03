8ff073f4162ca522d78cd5654a5b5266b381d964
illfate <illfate254@gmail.com>
2020-10-03 22:54:14 +0300

    Add producer consumer

--- 2/main.cpp
+++ 2/main.cpp
@@ -1,14 +1,23 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
+#include <thread>
 #include "catch.hpp"
+#include "queque.h"
 
-unsigned int Factorial(unsigned int number) {
-    return number <= 1 ? number : Factorial(number - 1) * number;
+void boo(ConsumersProducer<int> &cq) {
+    auto foo = [](int v) {
+        std::cout << v<<std::endl;
+    };
+    cq.Subscribe(foo);
 }
 
-TEST_CASE("Factorials are computed", "[factorial]") {
-    REQUIRE(Factorial(1) == 1);
-    REQUIRE(Factorial(2) == 2);
-    REQUIRE(Factorial(3) == 6);
-    REQUIRE(Factorial(10) == 3628800);
+TEST_CASE("Test producer consumer") {
+    ConsumersProducer<int> cq(2);
+
+    std::thread tr1(boo, std::ref(cq));
+    std::thread tr2(boo, std::ref(cq));
+    cq.Publish(1);
+    cq.StopPublishing();
+    tr1.join();
+    tr2.join();
 }
---
+++ 2/queque.h
@@ -0,0 +1,69 @@
+//
+// Created by illfate on 10/3/20.
+//
+
+#ifndef INC_2_QUEQUE_H
+#define INC_2_QUEQUE_H
+
+#include <queue>
+#include <mutex>
+#include <memory>
+#include <atomic>
+#include <condition_variable>
+
+template<typename T>
+class ConsumersProducer {
+private:
+    std::atomic<int> thread_counter{-1};
+    std::atomic<bool> is_publishing{true};
+    std::mutex mx;
+    std::vector<std::shared_ptr<std::queue<T>>> queues;
+    std::vector<bool> working_queues;
+    std::condition_variable cv;
+public:
+    explicit ConsumersProducer(int num_consumers) {
+        queues.reserve(num_consumers);
+        working_queues.reserve(num_consumers);
+        for (int i = 0; i < num_consumers; ++i) {
+            queues.push_back(std::make_shared<std::queue<T>>(std::queue<T>()));
+            working_queues.push_back(true);
+        }
+    }
+
+    void Publish(const T &v) {
+        for (auto &q:queues) {
+            std::unique_lock<std::mutex> lock(mx);
+            q->push(v);
+            cv.notify_all();
+        }
+    }
+
+    void StopPublishing() {
+        for (auto &q:queues) {
+            while (!q->empty()) {
+                std::unique_lock<std::mutex> lock(mx);
+                cv.notify_all();
+            }
+        }
+        is_publishing = false;
+    }
+
+    void Subscribe(std::function<void(T &v)> func) {
+        int cur_idx = ++thread_counter;
+        while (is_publishing) {
+            while (queues[cur_idx]->empty() && is_publishing) {
+                std::unique_lock<std::mutex> lock(mx);
+                cv.wait(lock);
+            }
+            if (!is_publishing) {
+                return;
+            }
+            std::unique_lock<std::mutex> lock(mx);
+            T value = queues[cur_idx]->front();
+            queues[cur_idx]->pop();
+            func(value);
+        }
+    }
+};
+
+#endif //INC_2_QUEQUE_H
