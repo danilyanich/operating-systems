5fd1804ceec28711292353088c281d4395499792
Masha <mzhirko@gmail.com>
2020-10-08 13:47:06 +0300

    Update test function calculator

--- 2/test.cpp
+++ 2/test.cpp
@@ -6,8 +6,22 @@
 #include <iostream>
 
 
-TEST_CASE("Test producer consumer") {
-    FunctionValuesCalculator calculator = FunctionValuesCalculator(<#initializer#>, <#initializer#>);
-    std::ofstream of("/home/illfate/CLionProjects/operating-systems/2/1.txt");
-    calculator.Process(of, std::cout);
+void testCalcFunc(const std::function<void(const Point &p)> &func) {
+    for (int i = 0; i < 2; i++) {
+        Point p{};
+        p.x = i;
+        p.y = i * 2;
+        func(p);
+    }
+}
+
+TEST_CASE("Test function calculator") {
+    FunctionValuesCalculator calculator = FunctionValuesCalculator(testCalcFunc);
+    std::stringstream write_ss;
+    std::stringstream log_ss;
+    calculator.Process(write_ss, log_ss);
+    std::string expected_write = "0 0\n2 1\n";
+    REQUIRE(expected_write == write_ss.str());
+    REQUIRE(log_ss.str().find("point: y: 0, x: 0,") != std::string::npos);
+    REQUIRE(log_ss.str().find("point: y: 2, x: 1,") != std::string::npos);
 }
