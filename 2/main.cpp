980fd4ad788dbe4a03b72679eef4da221e795673
Masha <mzhirko@gmail.com>
2020-10-08 13:26:09 +0300

    Pass calc func explicitly

--- 2/main.cpp
+++ 2/main.cpp
@@ -2,82 +2,12 @@
 // Created by illfate on 10/8/20.
 //
 
-#include <thread>
-#include <chrono>
-#include <fstream>
 #include <iostream>
-#include "consumer_producer.h"
+#include "function_calculator.h"
 
-
-struct Point {
-    double x;
-    double y;
-};
-
-struct PointWithSysInfo {
-    explicit PointWithSysInfo(Point p) : p(p) {}
-
-    Point p;
-    std::chrono::system_clock::time_point accept_time;
-    std::chrono::system_clock::time_point write_time;
-};
-
-
-class FunctionValuesCalculator {
-    ConsumersProducer<Point> calculator = ConsumersProducer<Point>(1);
-    ConsumersProducer<PointWithSysInfo> sys_info = ConsumersProducer<PointWithSysInfo>(1);
-    std::thread values_writer;
-    std::thread logging;
-public:
-    FunctionValuesCalculator() = default;
-
-    void Process() {
-        std::ofstream of("/home/illfate/CLionProjects/operating-systems/2/1.txt");
-
-        values_writer = std::thread(subscribe, std::ref(calculator), std::ref(sys_info), std::ref(of));
-        logging = std::thread(write_, std::ref(sys_info), std::ref(std::cout));
-        functionValuesProducer(calculator);
-        calculator.StopPublishing();
-        values_writer.join();
-        sys_info.StopPublishing();
-        logging.join();
-    }
-
-private:
-
-    static void subscribe(ConsumersProducer<Point> &cq, ConsumersProducer<PointWithSysInfo> &cq2, std::ostream &os) {
-        auto foo = [&cq2, &os](Point &v) {
-            PointWithSysInfo pointWithSysInfo(v);
-            pointWithSysInfo.accept_time = std::chrono::system_clock::now();
-            os << v.y << " " << v.x << "\n";
-            pointWithSysInfo.write_time = std::chrono::system_clock::now();
-            cq2.Publish(pointWithSysInfo);
-        };
-        cq.Subscribe(foo);
-    }
-
-    static void functionValuesProducer(ConsumersProducer<Point> &cp) {
-        for (int i = 0; i < 10; i++) {
-            Point p{};
-            p.x = i;
-            p.y = 10 * i * i;
-            cp.Publish(p);
-        }
-    }
-
-    static void write_(ConsumersProducer<PointWithSysInfo> &cq, std::ostream &os) {
-        auto foo = [&os](PointWithSysInfo &v) {
-            auto accept_time = std::chrono::system_clock::to_time_t(v.accept_time);
-            auto write_time = std::chrono::system_clock::to_time_t(v.write_time);
-            os << "point: y: " << v.p.y << ", x: " << v.p.x << ", accept time: " << std::ctime(&accept_time)
-               << "write to file time: "
-               << std::ctime(&write_time) << "\n";
-        };
-        cq.Subscribe(foo);
-    }
-};
 
 int main() {
-    FunctionValuesCalculator calculator = FunctionValuesCalculator();
-    calculator.Process();
+    FunctionValuesCalculator calculator(functionValuesProducer);
+    std::ofstream of("/home/illfate/CLionProjects/operating-systems/2/1.txt");
+    calculator.Process(of, std::cout);
 }
--- 2/test.cpp
+++ 2/test.cpp
@@ -1,68 +1,13 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include <thread>
 #include <chrono>
 #include "catch.hpp"
-#include "consumer_producer.h"
-#include <fstream>
+#include "function_calculator.h"
 #include <iostream>
 
 
-struct Point {
-    double x;
-    double y;
-};
-
-struct PointWithSysInfo {
-    explicit PointWithSysInfo(Point p) : p(p) {}
-
-    Point p;
-    std::chrono::system_clock::time_point accept_time;
-    std::chrono::system_clock::time_point write_time;
-};
-
-void subscribe(ConsumersProducer<Point> &cq, ConsumersProducer<PointWithSysInfo> &cq2, std::ostream &os) {
-    auto foo = [&cq2, &os](Point &v) {
-        PointWithSysInfo pointWithSysInfo(v);
-        pointWithSysInfo.accept_time = std::chrono::system_clock::now();
-        os << v.y << " " << v.x << "\n";
-        pointWithSysInfo.write_time = std::chrono::system_clock::now();
-        cq2.Publish(pointWithSysInfo);
-    };
-    cq.Subscribe(foo);
-}
-
-void functionValuesProducer(ConsumersProducer<Point> &cp) {
-    for (int i = 0; i < 10; i++) {
-        Point p{};
-        p.x = i;
-        p.y = 10 * i * i;
-        cp.Publish(p);
-    }
-}
-
-void write_(ConsumersProducer<PointWithSysInfo> &cq, std::ostream &os) {
-    auto foo = [&os](PointWithSysInfo &v) {
-        auto accept_time = std::chrono::system_clock::to_time_t(v.accept_time);
-        auto write_time = std::chrono::system_clock::to_time_t(v.write_time);
-        os << "point: y: " << v.p.y << ", x: " << v.p.x << ", accept time: " << std::ctime(&accept_time)
-           << "write to file time: "
-           << std::ctime(&write_time) << "\n";
-    };
-    cq.Subscribe(foo);
-}
-
 TEST_CASE("Test producer consumer") {
-    ConsumersProducer<Point> cp(1);
-
-    ConsumersProducer<PointWithSysInfo> cp2(1);
-
+    FunctionValuesCalculator calculator = FunctionValuesCalculator(<#initializer#>, <#initializer#>);
     std::ofstream of("/home/illfate/CLionProjects/operating-systems/2/1.txt");
-    std::thread tr1(subscribe, std::ref(cp), std::ref(cp2), std::ref(std::cout));
-    std::thread tr2(write_, std::ref(cp2), std::ref(of));
-    functionValuesProducer(cp);
-    cp.StopPublishing();
-    tr1.join();
-    cp2.StopPublishing();
-    tr2.join();
+    calculator.Process(of, std::cout);
 }
