d6d51f1655b9f59c2a238a8ecd4e2c2ac8781140
SHtress <stas.butrin@mail.ru>
2020-10-09 12:42:17 +0300

    feat():Add free function, change structure block

--- 1/src/main.c
+++ 1/src/main.c
@@ -1,51 +1,122 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 #include "memory/memory.h"
 
-int main(int argc, char **argv) {
-  m_init(1, 500);
 
-  int error_code;
+int main(int argc, char** argv) {
+	m_init(1, 500);
 
-  m_id chunk_1 = m_malloc(13, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	int error_code;
 
-  m_id chunk_2 = m_malloc(20, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	struct block* chunk_1 = m_malloc(23, &error_code);
+	if (error_code != M_ERR_OK) abort();
 
-  m_id chunk_3 = m_malloc(50, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	struct block* chunk_2 = m_malloc(33, &error_code);
+	if (error_code != M_ERR_OK) abort();
 
-  m_write(chunk_1, "Hello World!", 13, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	struct block* chunk_3 = m_malloc(23, &error_code);
+	if (error_code != M_ERR_OK) abort();
 
-  m_write(chunk_2, "Operating Systems", 18, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	struct block* chunk_4 = m_malloc(43, &error_code);
+	if (error_code != M_ERR_OK) abort();
 
-  m_write(chunk_3, "Super dumb memory allocator", 28, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	struct block* chunk_5 = m_malloc(23, &error_code);
+	if (error_code != M_ERR_OK) abort();
 
-  char buffer[50];
+	char buffer[100];
 
-  m_read(chunk_1, buffer, 13, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
+	m_write(*chunk_1, "Hello World!", 23, &error_code);
+	if (error_code != M_ERR_OK) abort();
 
-  m_read(chunk_2, buffer, 18, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
+	m_write(*chunk_2, "1234567890", 33, &error_code);
+	if (error_code != M_ERR_OK) abort();
 
-  m_read(chunk_3, buffer, 28, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
+	m_write(*chunk_3, "LAB COATS ARE AWESOME", 23, &error_code);
+	if (error_code != M_ERR_OK) abort();
 
-  m_free(chunk_1, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	m_write(*chunk_4, "EL PSY CONGROO", 43, &error_code);
+	if (error_code != M_ERR_OK) abort();
 
-  m_free(chunk_2, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	m_write(*chunk_5, "TEST", 23, &error_code);
+	if (error_code != M_ERR_OK) abort();
 
-  m_free(chunk_3, &error_code);
-  if (error_code != M_ERR_OK) abort();
+	printf("\n    BEFORE:    \n");
+	printf("----------FIRST CHUNK----------- \n");
+
+	m_read(*chunk_1, buffer, 23, &error_code);
+	if (error_code != M_ERR_OK) abort();
+	printf("%s\n", buffer);
+	memset(buffer, 0, sizeof(buffer));
+
+	printf("----------SECOND CHUNK--------- \n");
+
+	m_read(*chunk_2, buffer, 33, &error_code);
+	if (error_code != M_ERR_OK) abort();
+	printf("%s\n", buffer);
+	memset(buffer, 0, sizeof(buffer));
+
+	printf("---------THIRD CHUNK------- \n");
+
+	m_read(*chunk_3, buffer, 23, &error_code);
+	if (error_code != M_ERR_OK) abort();
+	printf("%s\n", buffer);
+	memset(buffer, 0, sizeof(buffer));
+
+	printf("---------FOURTH CHUNK------- \n");
+
+	m_read(*chunk_4, buffer, 43, &error_code);
+	if (error_code != M_ERR_OK) abort();
+	printf("%s\n", buffer);
+	memset(buffer, 0, sizeof(buffer));
+
+	printf("---------FIFTH CHUNK------- \n");
+
+	m_read(*chunk_5, buffer, 23, &error_code);
+	if (error_code != M_ERR_OK) abort();
+	printf("%s\n", buffer);
+	memset(buffer, 0, sizeof(buffer));
+
+	//-----------------------------------------------
+
+	m_free(chunk_2, &error_code, 33);
+
+	//-----------------------------------------------
+
+	printf("\n    AFTER:    \n");
+
+	printf("--------FIRST CHUNK--------- \n");
+
+	m_read(*chunk_1, buffer, 23, &error_code);
+	if (error_code != M_ERR_OK) abort();
+	printf("%s\n", buffer);
+	memset(buffer, 0, sizeof(buffer));
+
+	printf("----------SECOND CHUNK------\n");
+
+	m_read(*chunk_2, buffer, 33, &error_code);
+	if (error_code != M_ERR_OK) abort();
+	printf("%s\n", buffer);
+	memset(buffer, 0, sizeof(buffer));
+
+	printf("---------THIRD CHUNK------- \n");
+
+	m_read(*chunk_3, buffer, 23, &error_code);
+	if (error_code != M_ERR_OK) abort();
+	printf("%s\n", buffer);
+	memset(buffer, 0, sizeof(buffer));
+
+	printf("---------FOURTH CHUNK------- \n");
+
+	m_read(*chunk_4, buffer, 43, &error_code);
+	if (error_code != M_ERR_OK) abort();
+	printf("%s\n", buffer);
+	memset(buffer, 0, sizeof(buffer));
+
+	printf("---------FIFTH CHUNK------- \n");
+
+	m_read(*chunk_5, buffer, 23, &error_code);
+	if (error_code != M_ERR_OK) abort();
+	printf("%s\n", buffer);
+	memset(buffer, 0, sizeof(buffer));
 }
--- 1/src/memory/memory.c
+++ 1/src/memory/memory.c
@@ -1,48 +1,141 @@
 #include <stdlib.h>
-#include <string.h>
-
+#include <stdio.h>
 #include "memory.h"
-
 
 char* _g_allocator_memory = NULL;
 int _g_allocator_memory_size = 0;
 int _g_bytes_allocated = 0;
 
+struct block* top = NULL;
+struct block* begin = NULL;
 
-m_id m_malloc(int size_of_chunk, m_err_code* error) {
-  if (_g_bytes_allocated + size_of_chunk > _g_allocator_memory_size) {
-    *error = M_ERR_ALLOCATION_OUT_OF_MEMORY;
-    return NULL;
-  }
+void check(struct block checked_block) {
+	if (checked_block.start != NULL) {
+		printf(checked_block.start);
+		printf("\n");
+	}
+	else {
+		printf("START IS NULL\n");
+		printf("\n");
+	}
+	if (checked_block.prev != NULL) {
+		printf(checked_block.prev);
+		printf("\n");
+	}
+	else {
+		printf("PREV IS NULL\n");
+		printf("\n");
+	}
+};
 
-  _g_bytes_allocated += size_of_chunk;
+struct block* m_malloc(int size_of_chunk, m_err_code* error) {
+	if (_g_bytes_allocated + size_of_chunk > _g_allocator_memory_size) {
+		*error = M_ERR_ALLOCATION_OUT_OF_MEMORY;
+	}
 
-  *error = M_ERR_OK;
-  return _g_allocator_memory + _g_bytes_allocated;
+	_g_bytes_allocated += size_of_chunk;
+
+	char* ptr_new = _g_allocator_memory + _g_bytes_allocated;
+
+	struct block* tmp = malloc(sizeof(*tmp));
+	//tmp->size = size_of_chunk;
+	tmp->start = ptr_new;
+	tmp->prev = &(*top);
+	top = tmp;
+	if (begin == NULL) begin = tmp;
+	printf("%d \n", (int)tmp->start);
+	*error = M_ERR_OK;
+	return tmp;
 }
 
+void m_free(struct block* ptr, m_err_code* error, int size) {
+	if (ptr == NULL) { *error = M_ERR_ALREADY_DEALLOCATED; }
+	struct block* q = top;
+	struct block* prev = NULL;
+	struct block* prev2 = NULL;
+	struct block* tmp;
+	struct block* iter;
+	struct block* iter2;
+	char buffer[100];
+	int move;
+	
+	
 
-void m_free(m_id ptr, m_err_code* error) {
-  *error = M_ERR_OK;
+	if (ptr->start == top->start) {
+		tmp = top;
+		top = ptr->prev;
+		memset(tmp->start, 0, size);
+		//free(&(tmp->start));
+		return;
+	}
+
+	if (ptr->start == begin->start) {
+		tmp = begin;
+		iter = top;
+		iter2 = NULL;
+		while (q != NULL) {
+			//printf("%d \n", _g_bytes_allocated);
+			if (q->start == tmp->start) {
+				begin = prev;
+				printf("%d \n", (int)tmp->start - (int)_g_allocator_memory);
+				memcpy(tmp->start, prev->start, (int)tmp->start - (int)_g_allocator_memory);
+				//printf("%s\n", ((int)tmp->start+size));
+				//memset(top->start, 0, size);
+				//free(&(top->start));
+				return;
+			}
+			prev = q;
+			q = q->prev;
+		}
+	}
+
+	q = top;
+
+	while (q != NULL) {
+		//printf("%d \n", q->start);
+		if (q->start == ptr->start) {
+			iter = top;
+			printf("%s \n",q->start);
+			printf((int)q->start + 23);
+			printf("\n%d\n",_g_bytes_allocated);
+			printf("\n%d \n", _g_bytes_allocated - ((int)q->start - (int)_g_allocator_memory));
+			memcpy(buffer, prev->start, _g_bytes_allocated - ((int)q->start - (int)_g_allocator_memory));
+			printf(buffer);
+			memcpy(q->start,buffer, _g_bytes_allocated - ((int)q->start - (int)_g_allocator_memory));
+			//q->size = prev->size;
+			//free(&(top->start));
+		}
+		prev = q;
+		q = q->prev;
+	}
+
+	_g_bytes_allocated -= size;
+	*error = M_ERR_OK;
 }
 
+void m_read(struct block read_from_id, void* read_to_buffer, int size_to_read, m_err_code* error) {
+	//if (size_to_read > read_from_id.size) { *error = M_ERR_OUT_OF_BOUNDS; return; }
+	if (&read_from_id != NULL && read_from_id.start != NULL)
+	{
+		memcpy(read_to_buffer, read_from_id.start, size_to_read);
+	}
+	else { *error = M_ERR_INVALID_CHUNK; }
 
-void m_read(m_id read_from_id,void* read_to_buffer, int size_to_read, m_err_code* error) {
-  memcpy(read_to_buffer, read_from_id, size_to_read);
-  *error = M_ERR_OK;
+	*error = M_ERR_OK;
 }
 
+void m_write(struct block write_to_id, void* write_from_buffer, int size_to_write, m_err_code* error) {
+	if (&write_to_id == NULL) { *error = M_ERR_INVALID_CHUNK; }
 
-void m_write(m_id write_to_id, void* write_from_buffer, int size_to_write, m_err_code* error) {
-  memcpy(write_to_id, write_from_buffer, size_to_write);
-  *error = M_ERR_OK;
+	memcpy(write_to_id.start, write_from_buffer, size_to_write);
+	//if (size_to_write > write_to_id.size) { *error = M_ERR_OUT_OF_BOUNDS; return; }
+	*error = M_ERR_OK;
 }
-
 
 void m_init(int number_of_pages, int size_of_page) {
-  if (_g_allocator_memory != NULL) free(_g_allocator_memory);
+	if (_g_allocator_memory != NULL) free(_g_allocator_memory);
 
-  _g_allocator_memory_size = number_of_pages * size_of_page;
-  _g_allocator_memory = malloc(_g_allocator_memory_size);
-  _g_bytes_allocated = 0;
+	_g_allocator_memory_size = number_of_pages * size_of_page;
+	_g_allocator_memory = (char*)malloc(_g_allocator_memory_size);
+	_g_bytes_allocated = 0;
 }
