c5f822787c91ed229e33a1e7f92135de8a903cc6
Sharfrichter <syntol1911@gmail.com>
2020-10-12 18:19:33 +0300

    fix bug with deleting first element in list, add comments, improve write and read functions

--- 1/src/main.c
+++ 1/src/main.c
@@ -44,39 +44,31 @@ int main(int argc, char **argv) {
     m_write(chunk_5, "Boy next door", 14, &error_code);
     if (error_code != M_ERR_OK) abort();
 
+    m_init(1, 200);
 
 
 
-    m_free(&chunk_2, &error_code);
-
-    struct mem_chunk *chunk_6 = m_malloc(12, &error_code);
-    if (error_code != M_ERR_OK) abort();
-
-    m_write(chunk_6, "Leather man", 12, &error_code);
-    if (error_code != M_ERR_OK) abort();
-    debug();
-
-    /*m_read(chunk_1, buffer, 13, &error_code);
+   m_read(chunk_3, buffer, 28, &error_code);
    if (error_code != M_ERR_OK) abort();
    printf("%s\n", buffer);
-
-  m_read(chunk_2, buffer, 18, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
-
-
-  m_read(chunk_3, buffer, 28, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
+    /*
+     m_read(chunk_2, buffer, 18, &error_code);
+     if (error_code != M_ERR_OK) abort();
+     printf("%s\n", buffer);
 
 
-  m_read(chunk_4, buffer, 17, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
+     m_read(chunk_3, buffer, 28, &error_code);
+     if (error_code != M_ERR_OK) abort();
+     printf("%s\n", buffer);
 
-    m_read(chunk_5, buffer, 14, &error_code);
-    if (error_code != M_ERR_OK) abort();
-    printf("%s\n", buffer);*/
+
+     m_read(chunk_4, buffer, 17, &error_code);
+     if (error_code != M_ERR_OK) abort();
+     printf("%s\n", buffer);
+
+       m_read(chunk_5, buffer, 14, &error_code);
+       if (error_code != M_ERR_OK) abort();
+       printf("%s\n", buffer);*/
 
 
 
--- 1/src/memory/memory.c
+++ 1/src/memory/memory.c
@@ -13,11 +13,7 @@ int size=0;//size of chunks_list array
 //int deleted_size=0;//size of deleted_chunks array
 
 void debug(){
-        /*printf("%s\n",_g_allocator_memory);
-        printf("%s\n",_g_allocator_memory+chunks_list[0]->size);
-        printf("%s\n",_g_allocator_memory+chunks_list[0]->size+chunks_list[1]->size);*/
-        printf("%s\n", _g_allocator_memory+13+28+17+14);
-
+        printf("%s\n", _g_allocator_memory+18+28);
 }
 
 m_id m_malloc(int size_of_chunk, m_err_code* error) {
@@ -26,22 +22,21 @@ m_id m_malloc(int size_of_chunk, m_err_code* error) {
     return NULL;
   }
   struct mem_chunk* tmp=malloc(sizeof(struct mem_chunk));
-  if(mem_chunks==NULL){
+  if(mem_chunks==NULL){//initializing first element of list
       tmp->ptr=_g_allocator_memory+_g_bytes_allocated;
       tmp->size=size_of_chunk;
       tmp->next=NULL;
       mem_chunks = tmp;
-  } else{
-      struct mem_chunk* prev=mem_chunks;
-      struct mem_chunk* next=mem_chunks->next;
+  } else{//when add new element to list
+      struct mem_chunk* prev=mem_chunks;//variables for iterating through the list
+      struct mem_chunk* next=mem_chunks->next;//
       while (1){
-          if(next==NULL){
-              //printf("%d",1);
-              tmp->ptr=_g_allocator_memory+_g_bytes_allocated;
-              tmp->size=size_of_chunk;
-              tmp->next=NULL;
-              prev->next=tmp;
-              tmp->prev = prev;
+          if(next==NULL){//when the next element is not exist
+              tmp->ptr=_g_allocator_memory+_g_bytes_allocated;//allocation
+              tmp->size=size_of_chunk;//
+              tmp->next=NULL;//adding to the list
+              prev->next=tmp;//
+              tmp->prev = prev;//
               break;
           } else{
               prev=prev->next;
@@ -50,27 +45,34 @@ m_id m_malloc(int size_of_chunk, m_err_code* error) {
       }
 
   }
-  _g_bytes_allocated += size_of_chunk;
+  _g_bytes_allocated += size_of_chunk;//increase shift for next allocation
   *error = M_ERR_OK;
   return (m_id)tmp;
 }
 
 
 void m_free(m_id ptr, m_err_code* error) {
-    struct mem_chunk** tmp=ptr;//указатель на указаель чтобы можно было сам указатель указывать на NULL
-    if(*tmp==NULL){//проверка на то что мы удаляли
+    struct mem_chunk** tmp=ptr;
+    if(*tmp==NULL){//checking for previous allocation
         printf("%s","DEALLOCATED ALREADY");
         *error=M_ERR_ALREADY_DEALLOCATED;
         return;
     }
-    int begin_of_chunk=0;//переменная для сдвига
-    _g_bytes_allocated -= (*tmp)->size;
-    struct mem_chunk*next=(*tmp)->next;//удаление из стека
-    struct mem_chunk*prev=(*tmp)->prev;//
-    prev->next=next;//
-    next->prev = prev;//
-    free(*tmp);
-    *tmp = NULL;
+
+    int begin_of_chunk=0;//variable for shift
+    _g_bytes_allocated -= (*tmp)->size;//decrease size of allocated bytes to prevent gap in memory after new allocation
+    if(*tmp==mem_chunks){
+        mem_chunks = mem_chunks->next;
+        free(*tmp);
+        *tmp = NULL;
+    } else{
+        struct mem_chunk*next=(*tmp)->next;//deleting from list
+        struct mem_chunk*prev=(*tmp)->prev;//
+        prev->next=next;//
+        next->prev = prev;//
+        free(*tmp);
+        *tmp = NULL;
+    }
     struct mem_chunk*it=mem_chunks;
     while (it!=NULL){//rewriting of the array exclude deallocated chunk
         memcpy(_g_allocator_memory + begin_of_chunk, it->ptr, it->size);
@@ -84,37 +86,61 @@ void m_free(m_id ptr, m_err_code* error) {
 
 
 void m_read(m_id read_from_id,void* read_to_buffer, int size_to_read, m_err_code* error) {
-    struct mem_chunk* tmp=read_from_id;
-    if(tmp==NULL){
+    struct mem_chunk* it=mem_chunks;//iterator from the beginning of list
+    if(read_from_id==NULL){//checking for deallocation
         *error=M_ERR_INVALID_CHUNK;
+        printf("%s\n", "invalid chunk");
         return;
+    } else while (1){//checking for existing
+        if(it==NULL){
+            *error=M_ERR_INVALID_CHUNK;
+            printf("%s\n", "invalid chunk");
+            return;
+        } else if(it==read_from_id)
+            break;
+            it = it->next;
     }
-
-    if(tmp->size>size_to_read){
+    if(it->size>size_to_read){//checking for size
         *error=M_ERR_OUT_OF_BOUNDS;
+        printf("%s\n", "out of bounds");
         return;
     }
-    memcpy(read_to_buffer, tmp->ptr, size_to_read);
+    memcpy(read_to_buffer, it->ptr, size_to_read);
     *error = M_ERR_OK;
 }
 
 
 void m_write(m_id write_to_id, void* write_from_buffer, int size_to_write, m_err_code* error) {
-    struct mem_chunk* tmp=write_to_id;
-    if(tmp->size<size_to_write){
+    struct mem_chunk* it=mem_chunks;//iterator from the beginning of list
+    if(write_to_id==NULL){//checking for deallocation
+        *error=M_ERR_INVALID_CHUNK;
+        printf("%s\n", "invalid chunk");
+        return;
+    }else while (1){//checking for existing
+            if(it==NULL){
+                *error=M_ERR_INVALID_CHUNK;
+                printf("%s\n", "invalid chunk");
+                return;
+            } else if(it==write_to_id)
+                break;
+            it = it->next;
+        }
+    if(it->size<size_to_write){//checking for size
+        printf("%s\n", "out of bounds");
         *error=M_ERR_OUT_OF_BOUNDS;
         return;
     }
-    memcpy(tmp->ptr, write_from_buffer, size_to_write);
+    memcpy(it->ptr, write_from_buffer, size_to_write);
     *error = M_ERR_OK;
 }
 
 
 void m_init(int number_of_pages, int size_of_page) {
   if (_g_allocator_memory != NULL) free(_g_allocator_memory);
+  mem_chunks=NULL;
   _g_allocator_memory_size = number_of_pages * size_of_page;
   _g_allocator_memory = malloc(_g_allocator_memory_size);
   _g_bytes_allocated = 0;
-  mem_chunks=NULL;
+
 
 }
