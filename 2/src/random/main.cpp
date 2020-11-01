792faeca618a4282ca5918604dce9225a9c73a88
Alexandr Chudopal <alex.ch@core-tech.ru>
2020-11-01 16:14:44 +0300

    added write function

--- 2/src/random/main.cpp
+++ 2/src/random/main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <unistd.h>
+
 
 using namespace std;
 
 
 void enter_array(int*, int);
 void print_array(int[], int);
+void write_array(int[], int);
 
 
 int main(){
+    //pid_t fork();
+
     int size;
     int *arr = new int[size];
 
@@ -16,9 +22,11 @@ int main(){
     
     enter_array(arr, size);
     print_array(arr, size);
+    write_array(arr, size);
 
     return 0;
 }
+
 
 void enter_array(
     int *arr, 
@@ -38,5 +46,18 @@ void print_array(
 ){
     for(int i = 0; i < size; i++){
         cout << arr[i] << endl;
+    }
+}
+
+
+void write_array(int arr[], int size){
+    ofstream out;
+    out.open("array.txt");
+    
+    if(out.is_open()){
+        
+        for(int i = 0; i < size; i++){
+            out << arr[i] << endl;;
+        }
     }
 }
