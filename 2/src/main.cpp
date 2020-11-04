f86761a129dac92fce90cbb9e18c540d21d2206d
Brunner20 <43754656+Brunner20@users.noreply.github.com>
2020-11-04 23:40:26 +0300

    add timer and upd proc

--- 2/src/main.cpp
+++ 2/src/main.cpp
@@ -3,14 +3,15 @@
 #include <chrono>
 #include <iostream>
 #include <cstdio>
-#include "atlstr.h"
 #include <cstdlib>
 #include <windows.h>
 #include <vector>
 
 using namespace std;
+using namespace this_thread;
 
-TCHAR s[] = TEXT("C://Program Files (x86)/Notepad++/notepad++.exe");
+//TCHAR s[] = TEXT("C:\\Windows\\System32\\calc.exe");
+TCHAR s[] = TEXT("C:\\Windows\\System32\\notepad.exe");
 vector<PROCESS_INFORMATION> process_info;
 
 int CreateMyProcess(int);
@@ -28,14 +29,42 @@ int main(int argc, char* argv[])
      }*/
 
     for (int i = 0; i < 10; i++) {
-        CreateMyProcess(10);
+        CreateMyProcess(10000);//10s
+    }
+
+    sleep_for(chrono::seconds(15));
+}
+void CALLBACK timer_callback(
+    _In_  PVOID lpParameter,
+    _In_  BOOLEAN timer_condition) {
+
+    if (timer_condition) {
+        DWORD dwProcessID = reinterpret_cast<DWORD>(lpParameter);
+        cout << dwProcessID << endl;
+
+    }
+    else {
+        cout << "is closed" << endl;
+        CreateMyProcess(5000);
+    }
+
+}
+void run_timer(PROCESS_INFORMATION pi, int time) {
+
+    HANDLE ph = OpenProcess(SYNCHRONIZE, FALSE, pi.dwProcessId);
+    if (ph) {
+        HANDLE hNewHandle;
+        RegisterWaitForSingleObject(
+            &hNewHandle,
+            ph,
+            &timer_callback,
+            reinterpret_cast<void*>(pi.dwProcessId),
+            5000,
+            WT_EXECUTEONLYONCE);
     }
 }
 
 int CreateMyProcess(int time) {
-
-
-
 
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
@@ -59,5 +88,6 @@ int CreateMyProcess(int time) {
         return 1;
     }
     process_info.push_back(pi);
+    run_timer(pi, time);
     return 0;
 }
