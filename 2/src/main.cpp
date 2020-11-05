61abcc6ae3c31faf3aec05f44e592e25ac686609
Brunner20 <43754656+Brunner20@users.noreply.github.com>
2020-11-05 16:49:59 +0300

    add stop func

--- 2/src/main.cpp
+++ 2/src/main.cpp
@@ -15,39 +15,61 @@ TCHAR s[] = TEXT("C:\\Windows\\System32\\notepad.exe");
 vector<PROCESS_INFORMATION> process_info;
 
 int CreateMyProcess(int);
+void stop_processes();
 
 int main(int argc, char* argv[])
 {
-
-    /* if (argc != 3 ) {
-         printf("Argument error");
-         return -1;
-     }
-
-     for (int i = 0; i < atoi(argv[1]); i++) {
-         CreateProcess(atoi(argv[2]));
-     }*/
-
-    for (int i = 0; i < 10; i++) {
-        CreateMyProcess(10000);//10s
+   
+ 
+   /* if (argc != 3 ) {
+        printf("Argument error");
+        return -1;
     }
 
-    sleep_for(chrono::seconds(15));
+    for (int i = 0; i < atoi(argv[1]); i++) {
+        CreateProcess(atoi(argv[2]));
+    }*/
+
+    for (int i = 0; i < 10; i++) {
+        CreateMyProcess(10000);
+    }
+
+    atexit(stop_processes);
+    sleep_for(chrono::seconds(5));
+
 }
+
+void stop_process_by_id(DWORD id) {
+
+
+    HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, id);
+    TerminateProcess(hProcess,0);
+    CloseHandle(hProcess);
+
+}
+
+void stop_processes() {
+
+    for (int i = 0; i < process_info.size(); i++) {
+        stop_process_by_id(process_info[i].dwProcessId);
+    }
+}
+
+
 void CALLBACK timer_callback(
     _In_  PVOID lpParameter,
     _In_  BOOLEAN timer_condition) {
-
+    
     if (timer_condition) {
         DWORD dwProcessID = reinterpret_cast<DWORD>(lpParameter);
         cout << dwProcessID << endl;
-
+        stop_process_by_id(dwProcessID);
     }
     else {
         cout << "is closed" << endl;
         CreateMyProcess(5000);
     }
-
+  
 }
 void run_timer(PROCESS_INFORMATION pi, int time) {
 
@@ -59,13 +81,13 @@ void run_timer(PROCESS_INFORMATION pi, int time) {
             ph,
             &timer_callback,
             reinterpret_cast<void*>(pi.dwProcessId),
-            5000,
+            10000,
             WT_EXECUTEONLYONCE);
     }
 }
 
 int CreateMyProcess(int time) {
-
+  
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
 
@@ -74,20 +96,20 @@ int CreateMyProcess(int time) {
     ZeroMemory(&pi, sizeof(pi));
 
     if (!CreateProcess(nullptr,
-        s,
-        nullptr,
-        nullptr,
-        FALSE,
-        FALSE,
-        nullptr,
-        nullptr,
-        &si,
-        &pi)
-        ) {
+                        s,     
+                        nullptr,
+                        nullptr,
+                        FALSE,
+                        FALSE,
+                        nullptr,
+                        nullptr,
+                        &si,
+                        &pi)
+        ){
         printf("CreateProcess failed (%d).\n", GetLastError());
         return 1;
     }
     process_info.push_back(pi);
-    run_timer(pi, time);
+    run_timer(pi,time);
     return 0;
 }
