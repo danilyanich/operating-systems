d60db9ed79ef0a8ba00d6a88792f4f1a8edffa5c
nikkis <nikkis@list.ru>
2020-11-10 21:50:49 +0300

    add block structure

--- 1/src/memory/memory.h
+++ 1/src/memory/memory.h
@@ -12,6 +12,15 @@ typedef int m_err_code; // Error code of sandbox memory
 typedef void* m_id; // Identifier of sandbox memory chunk
 
 
+struct block {
+	m_id memory;
+	int size;
+	struct block* prev;
+	struct block* next;
+	int isNextNull;
+
+};
+
 // Allocates a chunk in sandbox memory
 // @param size_of_chunk Desired size in bytes to be allocated in sandbox memory
 // @param error_code [out] M_ERR_OK, M_ERR_ALLOCATION_OUT_OF_MEMORY
