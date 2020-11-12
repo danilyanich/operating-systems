fefb43de543b82ae55d1041cbb98a73f6036a793
nikkis <nikkis@list.ru>
2020-11-12 20:20:49 +0300

    add read, write

--- 1/src/memory/memory.c
+++ 1/src/memory/memory.c
@@ -96,15 +96,36 @@ void m_free(m_id ptr, m_err_code* error) {
 }
 
 
-void m_read(m_id read_from_id,void* read_to_buffer, int size_to_read, m_err_code* error) {
-  memcpy(read_to_buffer, read_from_id, size_to_read);
-  *error = M_ERR_OK;
+void m_read(m_id read_from_id, void* read_to_buffer, int size_to_read, m_err_code* error) {
+    struct block* ptr_new = read_from_id;
+
+    if (size_to_read > ptr_new->size) {
+        *error = M_ERR_OUT_OF_BOUNDS;
+        return;
+    }
+    if (&read_from_id != NULL && ptr_new->memory != NULL)
+    {
+        memcpy(read_to_buffer, ptr_new->memory, size_to_read);
+    }
+   
+    *error = M_ERR_OK;
 }
 
 
 void m_write(m_id write_to_id, void* write_from_buffer, int size_to_write, m_err_code* error) {
-  memcpy(write_to_id, write_from_buffer, size_to_write);
-  *error = M_ERR_OK;
+    struct block* ptr_new = write_to_id;
+
+    if (write_to_id == NULL) {
+        *error = M_ERR_INVALID_CHUNK;
+        return;
+    }
+
+    if (size_to_write > ptr_new->size) {
+        *error = M_ERR_OUT_OF_BOUNDS;
+        return;
+    }
+    memcpy(ptr_new->memory, write_from_buffer, size_to_write);
+    
 }
 
 
