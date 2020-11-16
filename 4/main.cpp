c9a4836ed79305d93aaac4662d8586363013dfa1
Brunner20 <43754656+Brunner20@users.noreply.github.com>
2020-11-16 22:34:01 +0300

    control memory changed

--- 4/main.cpp
+++ 4/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include  "scr/PhMemory.h"
+
 using namespace std;
+
 int main() {
     cout << "Hello, World!" << std::endl;
 
--- 4/scr/ControlM.h
+++ 4/scr/ControlM.h
@@ -10,7 +10,12 @@ class ControlM {
 public:
     PhMemory memory;
     explicit ControlM(PhMemory memory);
-
+    Block* allocateFile();
+    bool appendValueTo(Block* first, char v);
+    Block* findFreeIdx();
+    string readFile(Block* initial) const;
+    Block* copyFile(Block* idx);
+    static void deleteFile(Block* idx);
 };
 
 
