703e01479e83463b5dd0153989fb3f2e7ab6943b
Kirill Malenchyk <44575871+KMalen@users.noreply.github.com>
2020-11-19 00:39:13 +0300

    Update main.c

--- 1/src/main.c
+++ 1/src/main.c
@@ -1,51 +1,74 @@
+//
+//  main.c
+//  memory-manager
+//
+//  Created by Kiril Malenchik on 10/10/20.
+//
+
 #include <stdlib.h>
 #include <stdio.h>
 
 #include "memory/memory.h"
 
-int main(int argc, char **argv) {
-  m_init(1, 500);
 
-  int error_code;
+void errorAbort(int error_code);
 
-  m_id chunk_1 = m_malloc(13, &error_code);
-  if (error_code != M_ERR_OK) abort();
+int main(int argc, const char * argv[]) {
+    m_init(1,500);
+    int error_code;
+    
+    char buffer[50];
+    
+    m_id chunk_1 = m_malloc(501, &error_code);
+    errorAbort(error_code);
+    
+    m_free(chunk_1, &error_code);
+    errorAbort(error_code);
 
-  m_id chunk_2 = m_malloc(20, &error_code);
-  if (error_code != M_ERR_OK) abort();
+    m_free(chunk_1, &error_code);
+    errorAbort(error_code);
+    
+    m_read(chunk_1, buffer, 13, &error_code);
+    errorAbort(error_code);
+    printf("%s\n", buffer);
+    
+    m_write(chunk_1, "Hello World!", 13, &error_code);
+    errorAbort(error_code);
 
-  m_id chunk_3 = m_malloc(50, &error_code);
-  if (error_code != M_ERR_OK) abort();
+    
+//    m_read(chunk_1 + 1, buffer, 13, &error_code);
+//    errorAbort(error_code);
+    
+    m_id chunk_2 = m_malloc(20, &error_code);
+    errorAbort(error_code);
+    
+    m_write(chunk_2, "Operating Systems", 18, &error_code);
+    errorAbort(error_code);
+    
+    m_read(chunk_2, buffer, 18, &error_code);
+    errorAbort(error_code);
+    printf("%s\n", buffer);
+    
+    m_free(chunk_2, &error_code);
+    errorAbort(error_code);  
 
-  m_write(chunk_1, "Hello World!", 13, &error_code);
-  if (error_code != M_ERR_OK) abort();
+    m_id chunk_3 = m_malloc(50, &error_code);
+    errorAbort(error_code);
 
-  m_write(chunk_2, "Operating Systems", 18, &error_code);
-  if (error_code != M_ERR_OK) abort();
+    m_write(chunk_3, "Super dumb memory allocator", 28, &error_code);
+    errorAbort(error_code);
 
-  m_write(chunk_3, "Super dumb memory allocator", 28, &error_code);
-  if (error_code != M_ERR_OK) abort();
+    m_read(chunk_3, buffer, 28, &error_code);
+    errorAbort(error_code);
+    printf("%s\n", buffer);
 
-  char buffer[50];
+    m_free(chunk_3, &error_code);
+    errorAbort(error_code);
+}
 
-  m_read(chunk_1, buffer, 13, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
-
-  m_read(chunk_2, buffer, 18, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
-
-  m_read(chunk_3, buffer, 28, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
-
-  m_free(chunk_1, &error_code);
-  if (error_code != M_ERR_OK) abort();
-
-  m_free(chunk_2, &error_code);
-  if (error_code != M_ERR_OK) abort();
-
-  m_free(chunk_3, &error_code);
-  if (error_code != M_ERR_OK) abort();
+void errorAbort(int error_code){
+    
+    if (error_code != M_ERR_OK){
+        printf("Error: %d\n", error_code);
+    }
 }
