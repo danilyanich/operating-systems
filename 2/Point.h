48a96de43804c5c32f013fa1f0bf38a23a94cfcd
Valery1508 <58391822+Valery1508@users.noreply.github.com>
2020-11-19 22:19:51 +0300

    added Point.h

---
+++ 2/Point.h
@@ -0,0 +1,29 @@
+class Point {
+public:
+    int function_result;
+    bool deletable = false;
+    std::string function_result_time;
+    std::string record_result_time;
+
+    Point(int result_value, std::string function_result_time, std::string record_result_time) {
+        this->function_result = result_value;
+        this->function_result_time = function_result_time;
+        this->record_result_time = record_result_time;
+    }
+
+    ~Point() {
+        if (!this->deletable)
+            std::cout << "The point didn't logged yet\n";
+    }
+
+    std::string log_value() {
+        std::string record = "Function result: " + std::to_string(this->function_result) + ", calculated at " + this->function_result_time + " sec, recorded at " + this->record_result_time + " sec\n";
+        make_deletable();
+        return record;
+    }
+
+private:
+    void make_deletable() {
+        this->deletable = true;
+    }
+};
