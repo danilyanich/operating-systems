6248232fd1e64f1ca80835ab0360e86a400c145d
nikkis <nikkis@list.ru>
2020-11-20 13:22:25 +0300

    update point, add result writer

--- 2/src/Point.h
+++ 2/src/Point.h
@@ -12,5 +12,14 @@ public:
     }
 
 
+    std::string log_value() {
+        std::string record = "Function result: " + std::to_string(this->function_result) + ", calculated at " + this->function_result_time + " sec, recorded at " + this->record_result_time + " sec\n";
+        make_deletable();
+        return record;
+    }
 
+private:
+    void make_deletable() {
+        this->deletable = true;
+    }
 };
--- 2/src/main.cpp
+++ 2/src/main.cpp
@@ -6,6 +6,14 @@
 
 #include "Point.h"
 
+std::condition_variable cv;
+std::mutex mutex;
+bool notified_result_listener = false;
+bool notified_logger = false;
+bool done = false;
+int function_result = 1;
+int current_function_value = 1;
+
 std::fstream result_file;
 
 void init_file(char filename[]) {
@@ -19,6 +27,35 @@ void record_to_file(char filename[], std::string result) {
     result_file.close();
 }
 
+void result_writer() {
+    const char filename[] = "../results.txt";
+
+    init_file(const_cast<char*>(filename));
+
+    std::unique_lock<std::mutex> lock(mutex);
+
+    while (!done) {
+        while (notified_result_listener) {
+            record_to_file(const_cast<char*>(filename), std::to_string(function_result));
+            notified_result_listener = false;
+            notified_logger = true;
+            cv.notify_all();
+        }
+        cv.wait(lock);
+    }
+}
+
+void square_function(int digit) {
+    for (; current_function_value <= digit; current_function_value++) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        function_result = pow(current_function_value, 2);
+        notified_result_listener = true;
+        cv.notify_all();
+    }
+
+    done = true;
+    cv.notify_all();
+}
 
 int main() {
    
