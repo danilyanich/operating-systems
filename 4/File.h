c8a2b3ce5463ee0c4e3baa33bb21c80605189625
Nicola <kolya.vladimirsky@gmail.com>
2020-11-24 14:45:06 +0300

    task 4 variant 7

---
+++ 4/File.h
@@ -0,0 +1,29 @@
+//
+// Created by nicolas on 21/11/2020.
+//
+#pragma once
+
+#ifndef OS4_FILE_H
+#define OS4_FILE_H
+
+
+#include <string>
+#include <utility>
+#include "Table.h"
+
+class File
+{
+public:
+    explicit File(std::string  name) :
+        name(std::move(name))
+    {
+        index = Table::createChunk();
+    }
+    int index;
+    std::string name;
+    void write(char* buf)const;
+    std::string read()const;
+};
+
+
+#endif //OS4_FILE_H
---
+++ 4/Table.cpp
@@ -0,0 +1,26 @@
+//
+// Created by nicolas on 21/11/2020.
+//
+
+#include "Table.h"
+
+std::unique_ptr<Chunk>& Table::getChunk(int addres)
+{
+    return Table::table.at(addres);
+}
+
+int Table::createChunk()
+{
+    Table::table[indexes] = std::make_unique<Chunk>(indexes);
+    return indexes++;
+}
+
+void Table::delChunk(int addres)
+{
+    auto index = Table::table.at(addres).get();
+    if (!index->isLast)
+    {
+        delChunk(index->addres);
+    }
+    table.erase(addres);
+}
