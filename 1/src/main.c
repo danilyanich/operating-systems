97c3678d1714344282ba4d53991912a0e5c89b94
samoilovvvvv <44649842+samoilovvvvv@users.noreply.github.com>
2020-11-24 18:23:34 +0300

    add 1laba

--- 1/src/main.c
+++ 1/src/main.c
@@ -3,49 +3,96 @@
 
 #include "memory/memory.h"
 
-int main(int argc, char **argv) {
-  m_init(1, 500);
+void test(m_err_code error, char* test_name){
+    printf("Test \"%s\" started...\n", test_name);
+    switch(error) {
+        case M_ERR_OK:{
+            printf("Test \"%s\" PASSED\n", test_name);
+            break;
+        }
+        case M_ERR_INVALID_CHUNK:{
+            printf("Test \"%s\" EXIT: M_ERR_INVALID_CHUNK\n", test_name);
+            abort();
+        }
+        case M_ERR_OUT_OF_BOUNDS:{
+            printf("Test \"%s\" EXIT: M_ERR_OUT_OF_BOUNDS\n", test_name);
+            abort();
+        }
+        case M_ERR_ALREADY_DEALLOCATED:{
+            printf("Test \"%s\" WARNING: M_ERR_ALREADY_DEALLOCATED\n", test_name);
+            break;
+        }
+        case M_ERR_ALLOCATION_OUT_OF_MEMORY:{
+            printf("Test \"%s\" WARNING: M_ERR_ALLOCATION_OUT_OF_MEMORY\n", test_name);
+            break;
+        }
+        default:{
+            printf("Hello there!\n");
+        }
+    }
+    printf("\n");
+}
 
-  int error_code;
+int main() {
+    int error;
+    m_init(500);
+    segment_addr first = m_malloc(34, &error);
+    test(error, "TEST 1");
 
-  m_id chunk_1 = m_malloc(13, &error_code);
-  if (error_code != M_ERR_OK) abort();
+    segment_addr second = m_malloc(36, &error);
+    test(error, "TEST 2");
 
-  m_id chunk_2 = m_malloc(20, &error_code);
-  if (error_code != M_ERR_OK) abort();
+    segment_addr third = m_malloc(300, &error);
+    test(error, "TEST 3");
 
-  m_id chunk_3 = m_malloc(50, &error_code);
-  if (error_code != M_ERR_OK) abort();
+    segment_addr fourth = m_malloc(64, &error);
+    test(error, "TEST 4");
 
-  m_write(chunk_1, "Hello World!", 13, &error_code);
-  if (error_code != M_ERR_OK) abort();
+    test(m_free(fourth), "TEST 5");
 
-  m_write(chunk_2, "Operating Systems", 18, &error_code);
-  if (error_code != M_ERR_OK) abort();
+    //uncomment for M_ERR_ALLOCATION_OUT_OF_MEMORY
+    //segment_addr chunk_5 = m_malloc(555, &error);
+    //test(error, "TEST OUT OF MEMORY");
 
-  m_write(chunk_3, "Super dumb memory allocator", 28, &error_code);
-  if (error_code != M_ERR_OK) abort();
+    //uncomment for M_ERR_ALREADY_DEALLOCATED
+    //test(m_free(fourth), "TEST ALREADY DEALLOC");
 
-  char buffer[50];
+    //uncomment for M_ERR_INVALID_CHUNK
+    //int addr = 1111;
+    //test(m_free(&addr), "TEST INVALID CHUNK");
 
-  m_read(chunk_1, buffer, 13, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
+    //uncomment for M_ERR_OUT_OF_BOUNDS
+    //test(m_write(third, "ddd", 2222), "TEST BOUNDS 1");
 
-  m_read(chunk_2, buffer, 18, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
+    segment_addr chunk_1 = m_malloc(13, &error);
+    test(error, "TEST 6");
 
-  m_read(chunk_3, buffer, 28, &error_code);
-  if (error_code != M_ERR_OK) abort();
-  printf("%s\n", buffer);
+    segment_addr chunk_2 = m_malloc(20, &error);
+    test(error, "TEST 7");
 
-  m_free(chunk_1, &error_code);
-  if (error_code != M_ERR_OK) abort();
+    segment_addr chunk_3 = m_malloc(50, &error);
+    test(error, "TEST 8");
 
-  m_free(chunk_2, &error_code);
-  if (error_code != M_ERR_OK) abort();
+    test(m_write(chunk_1, "Hello World!", 13), "TEST 9");
 
-  m_free(chunk_3, &error_code);
-  if (error_code != M_ERR_OK) abort();
+    test(m_write(chunk_2, "Operating Systems", 18), "TEST 10");
+
+    test(m_write(chunk_3, "Super dumb memory allocator", 28), "TEST 11");
+
+    char* buffer = malloc(50);
+
+    test(m_read(chunk_1, buffer, 13), "TEST 12");
+
+    //uncomment for M_ERR_OUT_OF_BOUNDS
+    //test(m_read(chunk_1, buffer, 1111), "TEST BOUNDS 2");
+
+    test(m_read(chunk_2, buffer, 18), "TEST 13");
+
+    test(m_read(chunk_3, buffer, 28), "TEST 14");
+
+    test(m_free(chunk_1), "TEST 15");
+
+    test(m_free(chunk_2), "TEST 16");
+
+    test(m_free(chunk_3), "TEST 17");
 }
