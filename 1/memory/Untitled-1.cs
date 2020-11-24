c19d6c8f88fbda6aa8497e53f3ac7b10adbb7dd3
ulyanyunakh <44743429+ulyanyunakh@users.noreply.github.com>
2020-11-24 15:27:43 +0300

    lab 2 task 7

---
+++ 1/memory/Untitled-1.cs
@@ -0,0 +1,28 @@
+namespace UML
+{
+    public class C1
+    {
+        public string data1;
+        private int data2;
+        public C2[] data4;
+
+        public string Action1(C2 c2)
+        {
+            // какой-то код
+            string data5 = с2.Action2;
+            // какой-то код
+            return data5;
+        }
+    }
+
+    public class C2
+    {
+        public string data3;
+
+        public string Action2()
+        {
+            // какой-то код
+            return data3;
+        }
+    }
+}
---
+++ 2/thread_lab.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int visitors_count = 15;
+int chairs = 5;
+int queue_count = 0;
+
+queue<int> visitors_queue;
+mutex m;
+condition_variable c_v;
+
+void visitor() 
+{
+	int i = 0;
+	while (i < visitors_count)
+	{
+		this_thread::sleep_for(chrono::seconds(2));
+		cout << "came visitor " << i << endl;
+
+		unique_lock<mutex> lock(m);
+		if (queue_count < chairs)
+		{
+			cout << "visitor " << i << " in queue" << endl;
+			queue_count++;
+			visitors_queue.push(i);
+			c_v.notify_one();
+		}
+		else
+		{
+			cout << "no chairs: visitor " << i << " left" << endl;
+		}
+		lock.unlock();
+
+		i++;
+	}
+}
+
+void barber() 
+{
+	while (true) {
+		unique_lock<mutex> lock(m);
+		if (queue_count == 0)
+		{
+			cout << "barber sleeping" << endl;
+			c_v.wait(lock);
+			cout << "barber awake" << endl;
+		}
+		lock.unlock();
+
+		cout << "starting cuting visitor " << visitors_queue.front() << endl;
+		this_thread::sleep_for(chrono::seconds(5));
+
+		lock.lock();
+		cout << "visitor " << visitors_queue.front() << " cut" << endl;
+		cout << "visitor " << visitors_queue.front() << " left" << endl;
+		visitors_queue.pop();
+		queue_count--;
+		lock.unlock();
+	}
+}
+
+int main() 
+{
+	thread stylist(barber);
+	thread visitor(visitor);
+	stylist.join();
+	visitor.join();
+	return 0;
+}
