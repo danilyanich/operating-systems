b349ef5751837e359ff1ff527f0f0cba3be1f73e
OlegFrmMntk <Mankevich000@mail.ru>
2020-11-25 23:25:18 +0300

    sec commit

--- 1/src/main.c
+++ 1/src/main.c
@@ -1,39 +1,51 @@
-#include "memory/memory.h"
 #include <stdlib.h>
-#include "assert.h"
-#include <string.h>
+#include <stdio.h>
 
-void test_str_alloc(char* data) {
-    m_id ptr = NULL;
-    m_id* ptr2 = &ptr;
-    
-    m_malloc(ptr2, strlen(data));
-    m_write(*ptr2, data, strlen(data));
+#include "memory/memory.h"
 
-    void* resData = malloc(strlen(data));
-    m_read(*ptr2, resData, strlen(data));
-    assert(strcmp((char*)resData, data));
+int main(int argc, char **argv) {
+  m_init(1, 500);
 
-    m_free(*ptr2);
+  int error_code;
 
-    void* emptyData = malloc(strlen(data));
-    int res_code = m_read(*ptr2, emptyData, strlen(data));
-    assert(res_code == M_ERR_BAD_PARAMS);
-}
+  m_id chunk_1 = m_malloc(13, &error_code);
+  if (error_code != M_ERR_OK) abort();
 
-int main(int argc, char* argv[]) {
+  m_id chunk_2 = m_malloc(20, &error_code);
+  if (error_code != M_ERR_OK) abort();
 
-    int res = m_init(3, 10);
-    assert(res == 0);
+  m_id chunk_3 = m_malloc(50, &error_code);
+  if (error_code != M_ERR_OK) abort();
 
-    test_str_alloc((char*)"Hello");
-    test_str_alloc((char*)"world!");
-    test_str_alloc((char*)"Test");
-    test_str_alloc((char*)"word");
+  m_write(chunk_1, "Hello World!", 13, &error_code);
+  if (error_code != M_ERR_OK) abort();
 
-    m_clean();
+  m_write(chunk_2, "Operating Systems", 18, &error_code);
+  if (error_code != M_ERR_OK) abort();
 
-    printf("I'm OK!");
+  m_write(chunk_3, "Super dumb memory allocator", 28, &error_code);
+  if (error_code != M_ERR_OK) abort();
 
-    return 0;
+  char buffer[50];
+
+  m_read(chunk_1, buffer, 13, &error_code);
+  if (error_code != M_ERR_OK) abort();
+  printf("%s\n", buffer);
+
+  m_read(chunk_2, buffer, 18, &error_code);
+  if (error_code != M_ERR_OK) abort();
+  printf("%s\n", buffer);
+
+  m_read(chunk_3, buffer, 28, &error_code);
+  if (error_code != M_ERR_OK) abort();
+  printf("%s\n", buffer);
+
+  m_free(chunk_1, &error_code);
+  if (error_code != M_ERR_OK) abort();
+
+  m_free(chunk_2, &error_code);
+  if (error_code != M_ERR_OK) abort();
+
+  m_free(chunk_3, &error_code);
+  if (error_code != M_ERR_OK) abort();
 }
--- 1/src/memory/memory.h
+++ 1/src/memory/memory.h
@@ -1,92 +1,50 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
-#include <stdio.h>
-
-typedef char* m_id;							        // Тип описывающий адрес блока
-
-#define M_ERR_OK  0									// Успешное выполнение
-#define M_ERR_BAD_PARAMS -1							// Неверные параметры
-#define M_ERR_ALLOCATION_OUT_OF_MEMORY -2			// Недостаточно памяти
-#define M_ERR_UNKNOWN  1							// Неизвестная ошибка
-
-/**
-	@func	m_malloc
-	@brief	Выделяет блок памяти определенного размера
-	@param	[out] ptr				адресс блока
-	@param	[in]  size_of_block		размер блока
-	@return	код ошибки
-	@retval	0	успешное выполнение
-	@retval	-1	неверные параметры
-	@retval	-2	нехватка памяти
-	@retval	1	неизвестная ошибка
- **/
-int m_malloc(m_id* ptr, int size_of_block);
+#define M_ERR_OK 0 // Everything is ok
+#define M_ERR_ALLOCATION_OUT_OF_MEMORY 1 // Not enough memory for allocation
+#define M_ERR_ALREADY_DEALLOCATED 2 // The chunk was already deallocated
+#define M_ERR_INVALID_CHUNK 3 // The chunk is invalid, the operation did not succeed
+#define M_ERR_OUT_OF_BOUNDS 4 // The read/write operation out of bounds
 
 
-/**
-	@func	m_free
-	@brief	Удаление блока памяти
-	@param	[in] chunk_id	адресс блока
-	@return	код ошибки
-	@retval	0	успешное выполнение
-	@retval	-1	неверные параметры
-	@retval	1	неизвестная ошибка
- **/
-int m_free(m_id chunk_id);
+typedef int m_err_code; // Error code of sandbox memory
+typedef void* m_id; // Identifier of sandbox memory chunk
 
 
-/**
-	@func	m_read
-	@brief	Чтение информации из блока памяти
-	@param	[in] read_from_id		адресс блока
-	@param	[in] read_to_buffer		адресс буфера куда копируется инфомация
-	@param	[in] size_to_read		размер буфера
-	@return	код ошибки
-	@retval	0	успешное выполнение
-	@retval	-1	неверные параметры
-	@retval	-2	доступ за пределы блока
-	@retval	1	неизвестная ошибка
- **/
-int m_read(m_id read_from_id, void* read_to_buffer, int size_to_read);
+// Allocates a chunk in sandbox memory
+// @param size_of_chunk Desired size in bytes to be allocated in sandbox memory
+// @param error_code [out] M_ERR_OK, M_ERR_ALLOCATION_OUT_OF_MEMORY
+// @return An identifier for newly allocated chunk
+m_id m_malloc(int size_of_chunk, m_err_code* error_code);
 
 
-/**
-	@func	m_write
-	@brief	Запись информации в блок памяти
-	@param	[in] write_to_id		адресс блока
-	@param	[in] write_from_buffer	адресс буфера куда копируется инфомация
-	@param	[in] size_of_buffer		размер буфера
-	@return	код ошибки
-	@retval	0	успешное выполнение
-	@retval	-1	неверные параметры
-	@retval	-2	доступ за пределы блока
-	@retval	1	неизвестная ошибка
- **/
-int m_write(m_id write_to_id, void* write_from_buffer, int size_to_write);
+// Deallocates a chunk in sandbox memory
+// @param chunk_id Chunk identifier
+// @param error_code [out] M_ERR_OK, M_ERR_ALREADY_DEALLOCATED, M_ERR_INVALID_CHUNK
+void m_free(m_id chunk_id, m_err_code* error_code);
 
 
-/**
-	@func	m_init
-	@brief	Инициализация модели менеджера памяти
-	@param	[in] number_of_pages	количество страниц
-	@param	[in] size_of_page		размер страницы
-	В варианте 1 и 2 общий объем памяти расчитывается как number_of_pages * size_of_page
-	@return	код ошибки
-	@retval	0	успешное выполнение
-	@retval	-1	неверные параметры
-	@retval	1	неизвестная ошибка
- **/
-int m_init(int number_of_pages, int size_of_page);
+// Reads from chunk to a buffer
+// @param read_from_id The chunk to read from
+// @param read_to_buffer [out] The buffer to store data into
+// @param size_to_read Size of data in bytes to read from chunk
+// @param error_code [out] M_ERR_OK, M_ERR_INVALID_CHUNK, M_ERR_OUT_OF_BOUNDS
+void m_read(m_id read_from_id, void* read_to_buffer, int size_to_read, m_err_code* error_code);
 
-/**
-	@func	m_clean
-	@brief	Очищение памяти
-	@return	код ошибки
-	@retval	0	успешное выполнение
-	@retval	-1	неверные параметры
-	@retval	1	неизвестная ошибка
- **/
-int m_clean();
 
-#endif
+// Writes from buffer to a chunk
+// @param write_to_id The chunk to store data into
+// @param write_from_buffer The buffer to read from
+// @param size_to_write Size of data in bytes to be stored into chunk
+// @param error_code [out] M_ERR_OK, M_ERR_INVALID_CHUNK, M_ERR_OUT_OF_BOUNDS
+void m_write(m_id write_to_id, void* write_from_buffer, int size_to_write, m_err_code* error_code);
+
+
+// Initializes sandbox memory allocator. Usually it is number_of_pages*size_of_page.
+// @param number_of_pages Number of pages to allocate
+// @param size_of_page Size of the page
+void m_init(int number_of_pages, int size_of_page);
+
+
+#endif /* MEMORY_H */
