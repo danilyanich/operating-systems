bb8ea5bbb73bc15562eb80e418f8363f0117a091
mzhirko <mzhirko@gmail.com>
2020-11-26 22:09:19 +0300

    Add physical memory

---
+++ 4/src/Block.h
@@ -0,0 +1,19 @@
+//
+// Created by illfate on 11/26/20.
+//
+
+#ifndef FILE_SYSTEM_BLOCK_H
+#define FILE_SYSTEM_BLOCK_H
+
+
+#include <array>
+
+const size_t BLOCK_SIZE = 8;
+
+//using BlocksData = std::array<char, BLOCK_SIZE + 1>;
+
+//struct Block {
+//    BlocksData data;
+//};
+
+#endif //FILE_SYSTEM_BLOCK_H
