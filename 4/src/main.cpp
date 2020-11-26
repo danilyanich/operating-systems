37d075a75587bb56d52415d0905c8692533f33bb
mzhirko <mzhirko@gmail.com>
2020-11-27 02:51:12 +0300

    Add main example

--- 4/src/main.cpp
+++ 4/src/main.cpp
@@ -5,5 +5,17 @@
 
 
 int main() {
+    PhysicalMemory mem(std::string(100, ' '));
+    FileSystem fs(mem);
+    fs.CreateDir("dir1");
+    fs.CreateFile("file1", "body1");
+    fs.CreateFile("file2", "body2");
+    fs.ChangeDirectory("dir1");
+    fs.CreateFile("file3", "body3");
+    fs.CreateDir("dir2");
+    fs.CreateDir("dir3");
+    fs.ChangeDirectory("dir3");
+    fs.CreateFile("file4", "body4");
 
+    fs.Dump();
 }
