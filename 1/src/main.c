d06f2f7fde0699b55c0f6d5db1bf4e604cfb9524
KlevcevichAV <alexandr.klevcevich@gmail.com>
2020-12-09 12:12:58 +0300

    added first lab

--- 1/src/main.c
+++ 1/src/main.c
@@ -8,21 +8,24 @@ int main(int argc, char **argv) {
 
   int error_code;
 
-  m_id chunk_1 = m_malloc(13, &error_code);
+ 
+  struct block* chunk_1 = m_malloc(13, &error_code);
   if (error_code != M_ERR_OK) abort();
 
-  m_id chunk_2 = m_malloc(20, &error_code);
+  struct block* chunk_2 = m_malloc(20, &error_code);
   if (error_code != M_ERR_OK) abort();
 
-  m_id chunk_3 = m_malloc(50, &error_code);
+  struct block* chunk_3 = m_malloc(50, &error_code);
   if (error_code != M_ERR_OK) abort();
+
+ 
 
   m_write(chunk_1, "Hello World!", 13, &error_code);
   if (error_code != M_ERR_OK) abort();
 
   m_write(chunk_2, "Operating Systems", 18, &error_code);
   if (error_code != M_ERR_OK) abort();
-
+  
   m_write(chunk_3, "Super dumb memory allocator", 28, &error_code);
   if (error_code != M_ERR_OK) abort();
 
@@ -40,12 +43,22 @@ int main(int argc, char **argv) {
   if (error_code != M_ERR_OK) abort();
   printf("%s\n", buffer);
 
+ 
   m_free(chunk_1, &error_code);
   if (error_code != M_ERR_OK) abort();
 
-  m_free(chunk_2, &error_code);
+  struct block* chunk_4_1 = m_malloc(7, &error_code);
   if (error_code != M_ERR_OK) abort();
 
-  m_free(chunk_3, &error_code);
+  struct block* chunk_4_2 = m_malloc(8, &error_code);
+  if (error_code != M_ERR_OK) abort();
+
+  m_free(chunk_4_1, &error_code);
+  if (error_code != M_ERR_OK) abort();
+
+  struct block* chunk_5 = m_malloc(5, &error_code);
+  if (error_code != M_ERR_OK) abort();
+
+  struct block* chunk_6= m_malloc(5, &error_code);
   if (error_code != M_ERR_OK) abort();
 }
