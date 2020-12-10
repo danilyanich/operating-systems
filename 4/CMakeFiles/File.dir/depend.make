550f8ff87a8873235e9fbeb12fe4ab31e7ba6231
Elizabeth <kovel.liza@mail.ru>
2020-12-10 23:51:52 +0300

    Added 4 lab

---
+++ 4/CMakeFiles/File.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for File.
+# This may be replaced when dependencies are built.
---
+++ 4/CMakeFiles/FileSystem.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileSystem.dir/main.cpp.o"
+  "CMakeFiles/FileSystem.dir/command.cpp.o"
+  "CMakeFiles/FileSystem.dir/init.cpp.o"
+  "CMakeFiles/FileSystem.dir/load.cpp.o"
+  "CMakeFiles/FileSystem.dir/filecommand.cpp.o"
+  "CMakeFiles/FileSystem.dir/commonfunc.cpp.o"
+  "FileSystem.pdb"
+  "FileSystem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
