4d5412aa5532b100724a3a5e667c02a7d173d189
Ivan7111 <45482663+Ivan7111@users.noreply.github.com>
2020-12-15 12:12:24 +0300

    Added lab 1

--- 1/src/main.c
+++ 1/src/main.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <conio.h>
 
 #include "memory/memory.h"
 
 int main(int argc, char **argv) {
-  m_init(1, 500);
+  m_init(20, 20);
 
   int error_code;
 
@@ -17,16 +18,24 @@ int main(int argc, char **argv) {
   m_id chunk_3 = m_malloc(50, &error_code);
   if (error_code != M_ERR_OK) abort();
 
-  m_write(chunk_1, "Hello World!", 13, &error_code);
+  m_id chunk_4 = m_malloc(150, &error_code);
   if (error_code != M_ERR_OK) abort();
 
-  m_write(chunk_2, "Operating Systems", 18, &error_code);
+  m_write(chunk_1, "Salam Aleikum", 13, &error_code);
   if (error_code != M_ERR_OK) abort();
 
-  m_write(chunk_3, "Super dumb memory allocator", 28, &error_code);
+  m_write(chunk_2, "Dirizabl aga chita", 18, &error_code);
   if (error_code != M_ERR_OK) abort();
 
-  char buffer[50];
+  m_write(chunk_3, "Kono Giorno Giovanna yume ga aru", 32, &error_code);
+  if (error_code != M_ERR_OK) abort();
+
+  m_write(chunk_4, "Welcome to Camp Navarro. So, you're the new replacement... You are out of uniform, soldier! Where is your power armor?", 118, &error_code);
+  if (error_code != M_ERR_OK) abort();
+
+  char buffer[150];
+
+  printf("\nReading...\n\n");
 
   m_read(chunk_1, buffer, 13, &error_code);
   if (error_code != M_ERR_OK) abort();
@@ -36,9 +45,15 @@ int main(int argc, char **argv) {
   if (error_code != M_ERR_OK) abort();
   printf("%s\n", buffer);
 
-  m_read(chunk_3, buffer, 28, &error_code);
+  m_read(chunk_3, buffer, 32, &error_code);
   if (error_code != M_ERR_OK) abort();
   printf("%s\n", buffer);
+
+  m_read(chunk_4, buffer, 118, &error_code);
+  if (error_code != M_ERR_OK) abort();
+  printf("%s\n", buffer);
+
+  printf("\nReading complite\n\n");
 
   m_free(chunk_1, &error_code);
   if (error_code != M_ERR_OK) abort();
@@ -48,4 +63,9 @@ int main(int argc, char **argv) {
 
   m_free(chunk_3, &error_code);
   if (error_code != M_ERR_OK) abort();
+
+  m_free(chunk_4, &error_code);
+  if (error_code != M_ERR_OK) abort();
+
+  _getch();
 }
--- 1/src/memory/memory.c
+++ 1/src/memory/memory.c
@@ -1,33 +1,100 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "memory.h"
-
 
 char* _g_allocator_memory = NULL;
 int _g_allocator_memory_size = 0;
 int _g_bytes_allocated = 0;
 
+int page_size = 0;
+struct page *first_page;
+struct page *temporary_page = NULL;
 
 m_id m_malloc(int size_of_chunk, m_err_code* error) {
-  if (_g_bytes_allocated + size_of_chunk > _g_allocator_memory_size) {
-    *error = M_ERR_ALLOCATION_OUT_OF_MEMORY;
-    return NULL;
+  if(temporary_page != NULL){
+    if(temporary_page -> is_free == true && size_of_chunk <= page_size){
+      temporary_page -> is_free = false;
+      printf("Malloc complite\n");
+      *error = M_ERR_OK;
+      return temporary_page -> memory;
+    }
   }
 
-  _g_bytes_allocated += size_of_chunk;
-
-  *error = M_ERR_OK;
-  return _g_allocator_memory + _g_bytes_allocated;
+  struct page* page = first_page;
+  while(page -> next != NULL){
+    if(page -> is_free == true){
+      if(size_of_chunk <= page_size){
+        page -> is_free = false;
+        temporary_page = page;
+        printf("Malloc complite\n");
+        *error = M_ERR_OK;
+        return page -> memory;
+      }
+      else{
+        struct page* temp_page = page;
+        int previous_page_count = 1;
+        while(temp_page -> next -> is_free == true){
+          temp_page = temp_page -> next;
+          previous_page_count++;
+          if(size_of_chunk <= page_size * previous_page_count){
+            for(int i = 0; i < previous_page_count; i++){
+              page -> is_free = false;
+              if(i != 0) 
+                page -> previous = temp_page;
+              temp_page = page;
+              page = page -> next;
+            }
+            temporary_page = page;
+            printf("Malloc complite\n");
+            *error = M_ERR_OK;
+            return page -> memory;
+          }
+        }
+      }
+    }
+    page = page -> next;
+  }
+  *error = M_ERR_ALLOCATION_OUT_OF_MEMORY;
+  return NULL;
 }
-
 
 void m_free(m_id ptr, m_err_code* error) {
-  *error = M_ERR_OK;
+  if(temporary_page != NULL){
+    if(temporary_page -> memory == ptr){
+      temporary_page -> is_free = true;
+      struct page* page = temporary_page;
+      while(page -> previous != NULL){
+        page = page -> previous;
+        page -> is_free = true;
+      }
+      printf("Deleted\n");
+      *error = M_ERR_OK;
+      return;
+    }
+  }
+
+  struct page* page = first_page;
+  while(page -> next != NULL){
+    m_id t = page -> memory;
+    if(page -> memory == ptr){
+      page -> is_free = true;
+      while(page -> previous != NULL){
+        page = page -> previous;
+        page -> is_free = true;
+      }
+      temporary_page = page;
+      printf("Deleted\n");
+      *error = M_ERR_OK;
+      return;
+    }
+    page = page -> next;
+  }
 }
 
 
-void m_read(m_id read_from_id,void* read_to_buffer, int size_to_read, m_err_code* error) {
+void m_read(m_id read_from_id, void* read_to_buffer, int size_to_read, m_err_code* error) {
   memcpy(read_to_buffer, read_from_id, size_to_read);
   *error = M_ERR_OK;
 }
@@ -44,5 +111,28 @@ void m_init(int number_of_pages, int size_of_page) {
 
   _g_allocator_memory_size = number_of_pages * size_of_page;
   _g_allocator_memory = malloc(_g_allocator_memory_size);
-  _g_bytes_allocated = 0;
+  _g_bytes_allocated = size_of_page;
+
+  page_size = size_of_page;
+
+  struct page* page;
+  for(int i = 0; i < number_of_pages; i++){
+    if (i == 0){
+      page = (struct page*)malloc(sizeof(struct page));
+      first_page = page;
+    }
+    else 
+      page = page -> next;
+
+    page -> memory = _g_allocator_memory + _g_bytes_allocated;
+    page -> size = page_size;
+    page -> is_free = true;
+    page -> previous = NULL;
+
+    if(i == number_of_pages - 1) 
+      page -> next = NULL; 
+    else 
+      page -> next = (struct page*)malloc(sizeof(struct page));
+    _g_bytes_allocated += page_size;
+  }
 }
