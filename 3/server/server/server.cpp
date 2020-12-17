3b9e050f6705a70740c6a3b125d8206530afb77b
Ivan7111 <45482663+Ivan7111@users.noreply.github.com>
2020-12-17 16:21:59 +0300

    Added lab 3

---
+++ 3/server/server/server.cpp
@@ -0,0 +1,81 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+
+#include <WinSock2.h>
+#include <iostream>
+#include <windows.h>
+#include <thread>
+#include "windows.h"
+#include <fstream>
+
+#pragma warning(disable:4996)
+
+HANDLE mut;
+LPCWSTR str = L"HELLO";
+DWORD result;
+
+SOCKET Connections;
+
+void save_to_file(char msg[]) {
+    result = WaitForSingleObject(mut, INFINITE);
+
+    if (result == WAIT_OBJECT_0) {
+        std::ofstream fout;
+        fout.open("file_dump.txt", std::ios::app);
+
+        std::cout << "wait\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(4000));
+
+        fout << msg << std::endl;
+        fout.close();
+
+        ReleaseMutex(mut);
+    }
+}
+
+void print_() {
+    char msg[256];
+    while (true) {
+        recv(Connections, msg, sizeof(msg), NULL);
+        save_to_file(msg);
+    }
+}
+
+int main() {
+    mut = CreateMutex(NULL, FALSE, str);
+
+    WSADATA wsa_data;
+    WORD DLLVersion = MAKEWORD(2, 1);
+    if (WSAStartup(DLLVersion, &wsa_data) != 0) {
+        std::cout << "Error!\n";
+        exit(1);
+    }
+
+    SOCKADDR_IN addr;
+    int sizeofaddr = sizeof(addr);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(1111);
+    addr.sin_family = AF_INET;
+
+    SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+    bind(sListen, (SOCKADDR * ) & addr, sizeof(addr));
+    listen(sListen, SOMAXCONN);
+
+    SOCKET newConnection;
+    newConnection = accept(sListen, (SOCKADDR * ) & addr, &sizeofaddr);
+    if (newConnection == 0) {
+        std::cout << "Error2!\n";
+    } else {
+        char msg[256];
+        std::cout << "Create new connection!\n";
+        Connections = newConnection;
+        CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE) print_, NULL, NULL, NULL);
+    }
+
+
+    system("pause");
+
+    CloseHandle(mut);
+
+    return 0;
+
+}
---
+++ 3/solution/solution/solution.cpp
@@ -0,0 +1,51 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+
+#include <WinSock2.h>
+#include <iostream>
+#include <thread>
+#include <string>
+
+#pragma warning(disable:4996)
+
+int main() {
+    WSADATA wsa_data;
+    WORD DLLVersion = MAKEWORD(2, 1);
+    if (WSAStartup(DLLVersion, &wsa_data) != 0) {
+        std::cout << "Error!\n";
+        exit(1);
+    }
+
+    SOCKADDR_IN addr;
+    int sizeofaddr = sizeof(addr);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(1111);
+    addr.sin_family = AF_INET;
+
+    SOCKET connection = socket(AF_INET, SOCK_STREAM, NULL);
+    if (connect(connection, (SOCKADDR*)&addr, sizeofaddr) != 0) {
+        std::cout << "Error 2!\n";
+        return 1;
+    }
+
+    std::cout << "COnnected!!\n";
+    char msg[256];
+    for (int n = 0; n < 5; n++) {
+        int a = rand();
+        int b = rand();
+        int c = a + b;
+        std::string rel = std::to_string(c);
+        for (int i = 0; i < rel.size(); i++) {
+            msg[i] = rel[i];
+        }
+        for (int i = rel.size(); i < 256; i++) {
+            char v = char(" ");
+            msg[i] = v;
+        }
+        send(connection, msg, sizeof(msg), NULL);
+
+        Sleep(10);
+    }
+
+    system("pause");
+    return 0;
+}
