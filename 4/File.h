617c9ef25abfdd3c659d75daafaa12fa489ca307
monstrillllp <evgeniy27112000@gmail.com>
2020-12-19 19:16:39 +0300

    lb4-7

---
+++ 4/File.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef OS4_FILE_H
+#define OS4_FILE_H
+
+
+#include <string>
+#include <utility>
+#include "Table.h"
+
+class File
+{
+public:
+    explicit File(std::string  name) :
+        name(std::move(name))
+    {
+        index = Table::createChunk();
+    }
+    int index;
+    std::string name;
+    void write(char* buf)const;
+    std::string read()const;
+};
+
+
+#endif
