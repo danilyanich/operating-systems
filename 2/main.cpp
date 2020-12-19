946ed26d60655fbfa42bb1468e85915f01cc4cc6
monstrillllp <evgeniy27112000@gmail.com>
2020-12-19 19:01:51 +0300

    lb2-7

---
+++ 2/main.cpp
@@ -0,0 +1,15 @@
+#include <thread>
+#include "Manager.h"
+
+int main()
+{
+    Barber barber;
+    Manager manager{};
+    barber.init(3);
+    std::thread barber_thread(&Barber::startWorking, &barber);
+    std::thread manager_thread(&Manager::run, &manager, std::ref(barber));
+    std::thread manager_threa(&Manager::run, &manager, std::ref(barber));
+    barber_thread.join();
+    return 0;
+}
+`
