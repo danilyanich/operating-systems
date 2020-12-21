3d37f6edd6041ffd5403d39023ac7a0420171a49
fabishevskaya <fabishevskayavika@mail.ru>
2020-12-21 22:39:15 +0300

    add lab 4

---
+++ 4/cmake-build-debug/CMakeFiles/file_system.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vita/Загрузки/Telegram Desktop/file_system/file_system.cpp" "/home/vita/Загрузки/Telegram Desktop/file_system/cmake-build-debug/CMakeFiles/file_system.dir/file_system.cpp.o"
+  "/home/vita/Загрузки/Telegram Desktop/file_system/main.cpp" "/home/vita/Загрузки/Telegram Desktop/file_system/cmake-build-debug/CMakeFiles/file_system.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
